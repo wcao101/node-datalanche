40dc149b3864cb1a895d5c2daf454ad5469fd888
rpedela <rpedela@datalanche.com>
2013-05-15 13:48:40 -0600

    Add create/delete dataset and examples.

---
+++ examples/delete-dataset.js
@@ -0,0 +1,19 @@
+var dlanche = require('../lib');
+
+var client = dlanche.createClient({
+    key: '7zNN1Pl9SQ6lNZwYe9mtQw==',    // Add your API key.
+    secret: 'VCBA1hLyS2mYdrL6kO/iKQ==',  // Add your API secret.
+    host: 'localhost',
+    port: 4001,
+    verifySsl: false,
+});
+
+client.deleteDataset('test_dataset', function(err, request, response, list) {
+
+    if (err) {
+        console.log(JSON.stringify(err, null, '  '));
+        return;
+    }
+
+    console.log(JSON.stringify(list, null, '  '));
+});
